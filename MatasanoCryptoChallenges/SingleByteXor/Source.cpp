eb30145381ba75691ad1a977976da9a6b7d9ce4b
Alexandru Stanimir <alex.stanimir@live.com>
2015-11-08 23:54:56 +0200

    Updated visual studio C++headers

--- MatasanoCryptoChallenges/SingleByteXor/Source.cpp
+++ MatasanoCryptoChallenges/SingleByteXor/Source.cpp
@@ -7,13 +7,12 @@ using namespace std;
 // input :'	1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 // ouput : single char key
 
-
 static map<char, double> LetterFrequencies = {
-	{ 'a', 0.08167 }, { 'b', 0.01492 }, { 'c', 0.02782 }, { 'd', 0.04253 }, { 'e', 0.12702 },
-	{ 'f', 0.02228 }, { 'g', 0.02015 }, { 'h', 0.06094 }, { 'i', 0.06966 }, { 'j', 0.00153 },
-	{ 'k', 0.00772 }, { 'l', 0.04025 }, { 'm', 0.02406 }, { 'n', 0.06749 }, { 'o', 0.07507 },
-	{ 'p', 0.01929 }, { 'q', 0.00095 }, { 'r', 0.05987 }, { 's', 0.06327 }, { 't', 0.09056 },
-	{ 'u', 0.02758 }, { 'v', 0.00978 }, { 'w', 0.02360 }, { 'x', 0.00150 }, { 'y', 0.01974 },
+	{ 'a', 0.08167 },{ 'b', 0.01492 },{ 'c', 0.02782 },{ 'd', 0.04253 },{ 'e', 0.12702 },
+	{ 'f', 0.02228 },{ 'g', 0.02015 },{ 'h', 0.06094 },{ 'i', 0.06966 },{ 'j', 0.00153 },
+	{ 'k', 0.00772 },{ 'l', 0.04025 },{ 'm', 0.02406 },{ 'n', 0.06749 },{ 'o', 0.07507 },
+	{ 'p', 0.01929 },{ 'q', 0.00095 },{ 'r', 0.05987 },{ 's', 0.06327 },{ 't', 0.09056 },
+	{ 'u', 0.02758 },{ 'v', 0.00978 },{ 'w', 0.02360 },{ 'x', 0.00150 },{ 'y', 0.01974 },
 	{ 'z', 0.00074 } };
 unsigned char val(char c)
 {
@@ -46,7 +45,7 @@ double CalculateScore(string input)
 			continue;
 		result += LetterFrequencies.at(c);
 	}
-	return result;	
+	return result;
 }
 char bruteforceKey(string input)
 {
@@ -63,7 +62,7 @@ char bruteforceKey(string input)
 		{
 			xoredAscii += (c ^ i);
 		}
-		if (CalculateScore(xoredAscii)> score)
+		if (CalculateScore(xoredAscii) > score)
 		{
 			score = CalculateScore(xoredAscii);
 			decrypted = xoredAscii;
